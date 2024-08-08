92d58ee12a8bb38919433ea2a729ac893e48ba8f
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-08-08 10:45:26 +0300

    Minimum operations done

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" 0-minoperations"""
+
+
+def minOperations(n):
+    """
+    Gets fewest # of operations
+    """
+    # all outputs should be at least 2 char: (min, Copy All => Paste)
+    if (n < 2):
+        return 0
+    ops, root = 0, 2
+    while root <= n:
+        if n % root == 0:
+            ops += root
+            n = n / root
+            root -= 1
+        root += 1
+    return ops
